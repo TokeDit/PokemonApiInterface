16a58bd1436e43d68247fdbb1157d020ea2363c9
TokeDit <Tokedit11@gmail.com>
2026-03-20 09:58:24 +0100

    idk maybe we didnt push

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const baseUri = "https://pokeapi.co/api/v2/pokemon/?limit=100"
 
 
 Vue.createApp({
-    data(){
-        return{
+    Data() {
+        return {
             pokemons: [],
+            pokemonUrls: [],
             error: null,
         }
     },
-    async created(){
+    async created() {
         console.log("created method called")
         this.getPokemons(baseUri)
+        await this.getUrls(baseUri)
+        await this.getPhotos()
     },
     methods: {
-        async getPokemons(uri){
+        async getPokemons(uri) {
             try {
                 const response = await axios.get(uri)
                 this.pokemons = response.data.results
@@ -24,15 +27,28 @@ Vue.createApp({
                 this.error = ex.message
             }
         },
-        cleanList(){
+        async getUrls(uri) {
+            const response = await axios.get(uri)
+            this.pokemonUrls = response.data.results.map(pokemon => pokemon.url)
+            console.log(this.pokemonUrls)
+        },
+        async getPhotos() {
+            const requests = this.pokemonUrls.map(url => axios.get(url))
+            const responses = await Promise.all(requests)
+            this.pokemons = responses.map(response => ({
+                name: response.data.name,
+                image: response.data.sprites.front_default
+            }))
+        },
+        cleanList() {
             this.pokemons = []
             this.error = null
         },
-        async getByPokemonId(id){
-            if(id == null || id == ""){
+        async getByPokemonId(id) {
+            if (id == null || id == "") {
                 this.error == "There is no Pokemon with that ID";
                 this.pokemons = [];
-            } else{
+            } else {
                 const uri = baseUrl + "?userId=" + id;
                 this.getPokemons(uri);
             }
